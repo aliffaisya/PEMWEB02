7ef963d70bd9ec36f4bf52ea2fd4dbdda98dd2e0
aliffaisya <faisyafiana@gmail.com>
2024-02-21 16:06:59 +0700

    praktikum 1

---
+++ praktikum 1/variable.php
@@ -0,0 +1,13 @@
+<?php
+//Definisi variable
+$nama = "Aliffaisya Fiana";
+
+//memanggil variable
+echo $nama;
+echo "<br/>Umur = " . $umur." tahun";
+echo "<br/>Berat = " . $berat." Kg";
+
+echo "<br/>Hello $nama apa kabar";
+
+//penutup php berfungsi untuk menambahkan kode html pada kode php agar tidak eror
+?>
